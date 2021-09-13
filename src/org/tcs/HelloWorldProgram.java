e98103cb993a9f82e26725fb688e16da25e5dc2c
PremapriyaRavi <riyaaravi97@gmail.com>
2021-09-13 14:30:09 -0700

    Priya Code

--- src/org/tcs/HelloWorldProgram.java
+++ src/org/tcs/HelloWorldProgram.java
@@ -4,5 +4,7 @@ public class HelloWorldProgram {
 	public static void main(String[] args) {
 		System.out.println("Test 1");
 		System.out.println("Test 2");
+		System.out.println("Test 5");
+		System.out.println("Test 6");
 	}
 }
